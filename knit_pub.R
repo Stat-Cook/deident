5d4d66eb1bccd2aed938a153b1045225365286fe
Rob <rzc1@staff.staffs.ac.uk>
2023-07-21 14:56:21 +0100

    Add pub files

--- knit_pub.R
+++ knit_pub.R
@@ -1,24 +1,6 @@
 rmarkdown::render("paper.Rmd")
 
-preamble <- "---
-title: 'D-identifyR – open source tools for a data de-identification pipeline'
-tags:
-  - R
-  - de-identification
-  - data manipulation
-  - PID
-  - personally identifiable data
-authors:
-  - name: Robert M. Cook
-    orcid: 0000-0000-0000-0000
-    equal-contrib: true
-    affiliation: 1
-affiliations:
-  - name: Staffordshire University, Stafford, UK
-    index: 1
-date: 13 August 2017
-bibliography: paper.bib
----"
+preamble <- readr::read_lines("preamble.txt")
 
 c(preamble, readr::read_lines("paper.md")) |>
   readr::write_lines("paper.md")
