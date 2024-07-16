3b7008723b4e3ec109876b6af42531e0d0c76230
Robert Cook <robert.cook@bcu.ac.uk>
2024-07-16 11:45:03 +0100

    Implement user defined noise for perturber

--- R/pertruber.R
+++ R/pertruber.R
@@ -1,46 +1,70 @@
-white_noise <-  function(sd = 1){
-  #' @importFrom stats rnorm
+white_noise <- function(sd=0.1){
   function(values){
     rnorm(length(values), values, sd)
   }
 }
 
+lognorm_noise <-  function(sd = 0.1){
+  #' @importFrom stats rnorm
+
+  function(values){
+    values*rlnorm(length(values), 0, sd)
+  }
+  
+}
+
+adaptive_noise <-  function(sd.ratio = 1/10){
+  #' @importFrom stats rnorm
+
+  function(values){
+    .sd <- sd(values)
+    .sd <- .sd*sd.ratio
+    rnorm(length(values), values, .sd)
+  }
+  
+}
+
+
+initialize_perturb <- function(object, ...){
+  UseMethod("initialize_perturb")
+}
+
+initialize_perturb.function <- function(object, ...){
+  return(object)
+}
+
+initialize_perturb.numeric <- function(object, ...){
+  return(white_noise(object))
+}
+
+
 Perturber <- R6Class(
   #' @export
   "Perturber", list(
-    sd = NA,
+    noise.str = NA,
+    method = NA,
+    
+    
+    initialize = function(noise=1){
+      self$noise.str <- deparse(substitute(noise))
+      self$method = initialize_perturb(noise)
 
-    initialize = function(sd=1){
-      self$sd = sd
-      self$method = white_noise(sd)
     },
-
+    
     transform = function(keys, ...){
       keys <- c(keys, ...)
       self$method(keys)
     },
-
+    
     serialize = function(){
       super$serialize(on_init = list(sd = self$sd))
     },
-
+    
     str = function(...){
       super.str <- super$str()
-      glue("{super.str}(sd = {self$sd})")
+      glue("{super.str}({self$noise.str})")
     }
   ),
   inherit = BaseDeident
 )
 
-perturb <- function(data, perturber, ...){
-  UseMethod("perturb", perturber)
-}
-
-perturb.Perturber <- function(data, perturber, ...){
-  data |> perturber$mutate(...)
-}
-
-perturb.function <- function(data, func, ...){
-  pert <- Perturber$new(func)
-  data |> pert$mutate(...)
-}
