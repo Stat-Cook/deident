41ffddb27df275b385a1936a2debf007a63252dd
Rob <rzc1@staff.staffs.ac.uk>
2024-07-23 15:32:41 +0100

    add docstring

--- R/apply_deident.R
+++ R/apply_deident.R
@@ -1,9 +1,19 @@
+#' Apply a 'deident' pipeline
+#' 
+#' Applies a pipeline as defined by `deident` to a data frame. tibble, or file.
+#' 
+#' @param object The data to be deidentified
+#' @param deident A deidentification pipeline to be used.
+#' @param ... Terms to be passed to other methods
+#' 
+#' @export
 apply_deident <- function(object, deident, ...){
   UseMethod("apply_deident")
 }
 
 apply_deident.data.frame <- function(object, deident, ...){
   #' @exportS3Method
+  
   deident$mutate(object)
 }
 
