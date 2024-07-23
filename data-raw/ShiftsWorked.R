fe7595f3367750a7e78398ca2380c3359c66f45b
Rob <rzc1@staff.staffs.ac.uk>
2024-07-23 15:30:15 +0100

    Convert to tibble

--- data-raw/ShiftsWorked.R
+++ data-raw/ShiftsWorked.R
@@ -7,17 +7,17 @@ print("Remaking ShiftsWorked Data")
 
 {
   set.seed(101)
-
+  
   firstnames <- readr::read_csv("data-raw/new-top-firstNames.csv", col_types = "icd")
   firstnames <- stringr::str_to_sentence(firstnames$name)
   surnames <- readr::read_csv("data-raw/new-top-surnames.csv", col_types = "icd")
   surnames <- stringr::str_to_sentence(surnames$name)
-
+  
   individuals <- paste(sample(firstnames, 100, T), sample(surnames, 100, T))
-
+  
   pay_rate <- sample(c(rpois(25, 1000), rpois(40, 2000), rpois(25, 3000), rpois(10, 4000)))
   names(pay_rate) <- individuals
-
+  
   days <- seq(as.Date("2015-01-01"), as.Date("2015-01-31"), by="d")
   frm <- expand.grid(
     `Employee` = individuals,
@@ -26,12 +26,12 @@ print("Remaking ShiftsWorked Data")
   n <- nrow(frm)
   frm$`Record ID` <- seq(n)
   frm$Shift <- sample(c("Day", "Night","Day", "Night", "Rest"), n, T)
-
+  
   noise <- minutes(floor(rnorm(n, 0, 30)))
   start.times <- c(Day = as.POSIXct("2000-01-01 09:00:00", roll=T),
-    Night = as.POSIXct("2000-01-01 17:00:00", roll=T),
-    Rest = as.POSIXct("2000-01-01 00:00:00", roll=T))
-
+                   Night = as.POSIXct("2000-01-01 17:00:00", roll=T),
+                   Rest = as.POSIXct("2000-01-01 00:00:00", roll=T))
+  
   ShiftsWorked <- frm %>% mutate(
     Employee = as.character(Employee),
     `Shift Start` = start.times[Shift] + noise,
@@ -44,10 +44,12 @@ print("Remaking ShiftsWorked Data")
     `Daily Pay` = ceiling(`Hours Done` * `Hourly Rate`) / 100
   ) %>%
     select(`Record ID`, `Employee`, `Date`, `Shift`, `Shift Start`, `Shift End`, `Daily Pay`)
-
+  
   ShiftsWorked[ShiftsWorked$Shift == "Rest",]$`Shift Start` <- NA
   ShiftsWorked[ShiftsWorked$Shift == "Rest",]$`Shift End` <- NA
   ShiftsWorked[ShiftsWorked$Shift == "Rest",]$`Daily Pay` <- 0
+  
+  ShiftsWorked <- tibble(ShiftsWorked)
 }
 
 usethis::use_data(ShiftsWorked, overwrite = TRUE)
