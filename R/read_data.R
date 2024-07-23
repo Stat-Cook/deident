ec6841dba5a0996e6f00d3bddd7fb78094b25468
Rob <rzc1@staff.staffs.ac.uk>
2024-07-23 15:35:50 +0100

    Add import statements

--- R/read_data.R
+++ R/read_data.R
@@ -1,8 +1,3 @@
-#' read_data <- function(file_path, ...){
-#'   #' @export
-#'   UseMethod("read_data")
-#' }
-
 set_path_class <- function(file_path){
   ext <- tolower(tools::file_ext(file_path))
 
@@ -16,19 +11,23 @@ load_file <- function(file_path){
 }
 
 load_file.csv_path <- function(file_path, ...){
-  readr::read_csv(file_path, ...)
+  #' @importFrom readr read_csv
+  read_csv(file_path, ...)
 }
 
 load_file.xls_path <- function(file_path, ...){
-  openxlsx::read.xlsx(file_path,  ...)
+  #' @importFrom openxlsx read.xlsx
+  read.xlsx(file_path,  ...)
 }
 
 load_file.xlsx_path <- function(file_path, ...){
-  openxlsx::read.xlsx(file_path,  ...)
+  #' @importFrom openxlsx read.xlsx
+  read.xlsx(file_path,  ...)
 }
 
 load_file.tsv_path <- function(file_path, ...){
-  readr::read_tsv(file_path,  ...)
+  #' @importFrom readr read_tsv
+  read_tsv(file_path,  ...)
 }
 
 read_data <- function(file_path, ...){
@@ -37,26 +36,3 @@ read_data <- function(file_path, ...){
   load_file(file_path, ...)
 }
 
-#' read_data.csv_path <- function(file_path, ...){
-#'   #' @exportS3Method
-#'   frm <- read.csv(file_path, check.names = F, ...)
-#'   fix_colnames(frm)
-#' }
-#'
-#' read_data.xls_path <- function(file_path, ...){
-#'   #' @importFrom readxl read_xls
-#'   #' @exportS3Method
-#'   readxl::read_excel(file_path, ...)
-#' }
-#'
-#' read_data.xlsx_path <- function(file_path, ...){
-#'   #' @importFrom readxl read_xls
-#'   #' @exportS3Method
-#'   readxl::read_excel(file_path, ...)
-#' }
-#'
-#' read_data.tsv_path <- function(file_path, ...){
-#'   #' @exportS3Method
-#'   frm <- read.csv(file_path, sep = "\t", check.names = F, ...)
-#'   fix_colnames(frm)
-#' }
