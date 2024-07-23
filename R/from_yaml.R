e7a965f870612b693e4ac090de38f2231069ab66
Rob <rzc1@staff.staffs.ac.uk>
2024-07-23 17:23:21 +0100

    export 'from_yaml' function

--- R/from_yaml.R
+++ R/from_yaml.R
@@ -7,8 +7,24 @@ add_deident <- function(dlist, to_add){
 }
 
 
-from_yaml <- function(path, data=NULL){
-  #' @importFrom yaml read_yaml
+#' Restore a serialized deident from file
+#' 
+#' @param path Path to serialized deident.
+#' 
+#' @export
+#' 
+#' @examples
+#' 
+#' deident <- deident(ShiftsWorked, Pseudonymizer, Employee)
+#' deident$to_yaml("model.yml")
+#' 
+#' deident.yaml <- from_yaml("model.yml")
+#' deident.yaml$mutate(ShiftsWorked)
+#' 
+#' 
+#' @importFrom yaml read_yaml
+from_yaml <- function(path){
+  
   yml <- read_yaml(path)
   deident(yml)
 }
