5513ff29a6e1fa40f6d16b76375cc3aa1230d9b7
Rob <rzc1@staff.staffs.ac.uk>
2024-07-23 15:38:15 +0100

    add docstrings

--- R/shuffler.R
+++ R/shuffler.R
@@ -1,26 +1,43 @@
+#' Shuffler class for applying 'shuffling' transform
+#'
+#' 'Shuffling' refers to the a random sampling of a variable without
+#' replacement e.g. \[A, B, C\] becoming \[B, A, C\] but not \[A, A, B\].
+#' Shuffling will preserve top level metrics (e.g. mean, median, mode) but
+#' removes ordinal properties i.e. correlations and auto-correlations
+#'
+#' @param method \[optional\] A function representing the method of re-sampling
+#'   to be used.  By default uses exhaustive sampling without replacement.
+#' @param keys Value(s) to be transformed.
+#' @param ... Value(s) to concatenate to `keys` and transform
+#'  @inheritParams Pseudonymizer 
+#' @param limit integer - the minimum number of observations a variable needs to 
+#' have for shuffling to be performed.  If the variable has length less than `limit`
+#' values are repalced with `NA`s. 
+#'   
+#' @export
 Shuffler <- R6Class(
-  #' Shuffler class for applying 'shuffling' transform
-  #'
-  #' 'Shuffling' refers to the a random sampling of a variable without
-  #' replacement e.g. [A, B, C] becoming [B, A, C] but not [A, A, B].
-  #' Shuffling will preserve top level metrics (e.g. mean, median, mode) but
-  #' removes ordinal properties i.e. correlations and auto-correlations
-  #'
-  #' @param method [optional] A function representing the method of re-sampling
-  #'   to be used.  By default uses exhaustive sampling without replacement.
-  #'
-  #' @export
+
   "Shuffler", list(
+    #' @field limit minimum vector length to be shuffled.  If vector to be 
+    #' transformed has length < limit, the data is replaced with NAs
     limit = 0,
-    initialize = function(method=sample, limit=0){
-      self$set_method(method)
+    
+    #' @description 
+    #' Create new Shuffler object
+    initialize = function(limit=0){
+      self$set_method(sample)
       self$set_limit(limit)
     },
-
+    
+    #' @description 
+    #' Update minimum vector size for shuffling
     set_limit = function(limit){
       self$limit = limit
     },
 
+    #' @description  
+    #' `r transform.desc()`
+    #' 
     transform = function(keys, ...){
       keys <- c(keys, ...)
 
@@ -32,8 +49,10 @@ Shuffler <- R6Class(
       return(self$method(keys))
     },
 
-    serialize = function(dots=NA){
-      super$serialize(on_init = list(limit=self$limit), dots=dots)
+    #' @description 
+    #' `r serialize.desc()`
+    serialize = function(){
+      super$serialize(limit=self$limit)
     }
 
   ),
