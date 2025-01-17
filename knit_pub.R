b32b7b46661e2253cd596c9a30447b8a5852dad3
Rob <rzc1@staff.staffs.ac.uk>
2025-01-17 09:52:06 +0000

    Fixed DOI docs

--- knit_pub.R
+++ knit_pub.R
@@ -1,6 +1,10 @@
 rmarkdown::render("paper.Rmd")
 
+#knitr::knit("paper.Rmd")
+
 preamble <- readr::read_lines("preamble.txt")
 
 c(preamble, readr::read_lines("paper.md")) |>
+  stringr::str_replace_all("\\\\\\[", "\\[") |>
+  stringr::str_replace_all("\\\\\\]", "\\]") |>
   readr::write_lines("paper.md")
